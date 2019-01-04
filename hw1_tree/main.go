c9ee93605002f534f97836d1e625fd5b5ead807d
ksneo <vkashapov@gmail.com>
2019-01-04 11:08:34 +0300

    Add second task

--- hw1_tree/main.go
+++ hw1_tree/main.go
@@ -6,7 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
+	"sort"
 )
 
 const treeNode = "├"
@@ -14,39 +14,65 @@ const treeNodeLast = "└"
 const treeHor = "───"
 const treeVert = "│"
 
-func drawFilePath(file string, level int, isLast bool) string {
+func filter(vs []os.FileInfo, printFiles bool) []os.FileInfo {
+	vsf := make([]os.FileInfo, 0)
+	for _, v := range vs {
+		if !v.IsDir() && !printFiles {
+			continue
+		}
+		vsf = append(vsf, v)
+	}
+	return vsf
+}
+
+func drawFilePath(file string, level int, isLast bool, drawPathString string) string {
 	treeNodeString := treeNode
 	if isLast {
 		treeNodeString = treeNodeLast
 	}
-	return strings.Repeat(treeVert+"\t", level) + treeNodeString + treeHor + file
+	return drawPathString + treeNodeString + treeHor + file
 }
 
 func walkTree(out io.Writer, path string, printFiles bool, level int, drawPathString string) error {
 	files, err := ioutil.ReadDir(path)
+	files = filter(files, printFiles)
+	sort.Slice(files, func(i, j int) bool { return files[i].Name() < files[j].Name() })
+	curPathString := ""
+	newPathString := ""
 	if err != nil {
 		return err
 	}
 	elemCount := len(files)
 	for idx, file := range files {
 		if file.IsDir() {
-			fmt.Fprintln(out, drawPathString+file.Name())
-			err = walkTree(out, filepath.Join(path, file.Name()), printFiles, level+1, drawPathString)
+			if idx+1 == elemCount {
+				curPathString = treeNodeLast
+			} else {
+				curPathString = treeNode
+			}
+			curPathString = curPathString + treeHor
+			fmt.Fprintln(out, drawPathString+curPathString+file.Name()+"	")
+			if idx+1 == elemCount {
+				newPathString = drawPathString + " 	"
+			} else {
+				newPathString = drawPathString + treeVert + "	"
+			}
+			err = walkTree(out, filepath.Join(path, file.Name()), printFiles, level+1, newPathString)
 			if err != nil {
 				return err
 			}
 		} else if printFiles {
-			fmt.Fprintln(out, drawFilePath(file.Name(), level, idx+1 == elemCount))
+			fmt.Fprintln(out, drawFilePath(file.Name(), level, idx+1 == elemCount, drawPathString))
 		}
 	}
-
 	return nil
 }
 
 func dirTree(out io.Writer, path string, printFiles bool) error {
-	drawPathString := treeNode + treeHor
+	drawPathString := ""
 	level := 0
-	return walkTree(out, path, printFiles, level, drawPathString)
+	walkTree(out, path, printFiles, level, drawPathString)
+	return nil
 }
 
 func main() {
